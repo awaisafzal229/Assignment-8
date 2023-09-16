7697d6bba70584347ee8165ced3c9dd062e36c5a
awaisafzal229 <129833448+awaisafzal229@users.noreply.github.com>
2023-09-16 23:24:19 +0500

    more update

--- assignment8.js
+++ assignment8.js
@@ -3,8 +3,8 @@
 var _a, _b;
 // I have installed the Node.js, TypeScript and VS Code in my computer.
 // Q2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-// let nameOfPerson:string= "Ali";
-// console.log(`"Hello ${nameOfPerson}, would you like to to learn some Python today?"`);
+let nameOfPerson = "Ali";
+console.log(`"Hello ${nameOfPerson}, would you like to to learn some Python today?"`);
 // Q3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 let nameOfPerson = "ALI HAIDER";
 let cases = nameOfPerson.toLowerCase();
